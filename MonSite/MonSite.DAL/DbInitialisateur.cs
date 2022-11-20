d543e579b535dc2bd9da6288e8ecff112c182a86
Melek Ghouma <ghoumamelek@gmail.com>
2022-11-20 13:49:07 +0100

    Framework Core Création Base de Données après tuto

--- MonSite/MonSite.DAL/DbInitialisateur.cs
+++ MonSite/MonSite.DAL/DbInitialisateur.cs
@@ -3,16 +3,71 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MonSite.BL;
 
 namespace MonSite.DAL
 {
-    internal class DbInitialisateur
+    internal class DbInitializer
     {
-        private readonly
-        
-        //DbInitializer
+        private readonly ModelBuilder modelBuilder;
+        public DbInitializer(ModelBuilder modelBuilder)
+        {
+            this.modelBuilder = modelBuilder;
+        }
+        public void Seed()
+        {
+            modelBuilder.Entity<Blog>().HasData(
+            new Blog
+            {
 
-        //Seed()
+                BlogId = 1,
+
+                Url = "http://blogs.packtpub.com/dotnet"
+
+            },
+            new Blog
+            {
+
+                BlogId = 2,
+
+                Url = "http://blogs.packtpub.com/dotnetcore"
+
+            }
+            );
+            modelBuilder.Entity<Post>().HasData(
+            new Post
+            {
+                PostId = 1,
+                BlogId = 1,
+                Title = "Dotnet 4.7 Released",
+                Content = "Dotnet 4.7 Released Contents",
+                PublishedDate = DateTime.Now
+            },
+            new Post
+            {
+
+                PostId = 2,
+                BlogId = 1,
+                Title = ".NET Core 1.1 Released",
+                Content = ".NET Core 1.1 Released Contents",
+                PublishedDate = DateTime.Now
+
+            },
+            new Post
+            {
+
+                PostId = 3,
+
+                Title = "EF Core 1.1 Released",
+                BlogId = 2,
+                Content = "EF Core 1.1 Released Contents",
+                PublishedDate = DateTime.Now
+
+            }
+            );
+
+        }
     }
 
 }
--- MonSite/MonSite.DAL/MonSiteContext.cs
+++ MonSite/MonSite.DAL/MonSiteContext.cs
@@ -13,11 +13,16 @@ namespace MonSite.DAL
         public DbSet<Blog> Blogs { get; set; }
         public DbSet<Post> Posts { get; set; }
         public DbSet<Comment> Comments { get; set; }
+
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"server=(LocalDB)\MSSQLLocalDB; Initial Catalog=BDENIT2022;Integrated Security=true");
         }
 
-        //OnModeleCreating
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            new DbInitializer(modelBuilder).Seed();
+        }
     }
 }
