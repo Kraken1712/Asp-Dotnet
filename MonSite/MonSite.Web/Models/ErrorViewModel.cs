a6186a294ac5d95c4a30409c3d557f64b3b37ffc
Aziz <mohamedaziz.jaziri@etudiant-enit.utm.tn>
2022-12-20 18:48:18 +0100

    ajout class 1 : test

--- MonSite/MonSite.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace MonSite.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
